34f6e5a2ebbffe72b1c1d38d158d5c2c1ba72e71
TheroneAlmadin <102451950+therone18@users.noreply.github.com>
2023-10-01 13:24:37 +0800

    Skeleton

--- grades-table/src/App.js
+++ grades-table/src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
-
+import "./App.css";
+import { tableData } from "./data";
 function App() {
   return (
-    <div>
-      Hello World
+    <div class="main">
+      <div class="table-container">
+        <div>search bar component here</div>
+        <div>table component here</div>
+        <div>QPI Componenent here</div>
+      </div>
+      <div class="form-container">
+        <div class="form">
+          <div>input boxes here</div>
+        </div>
+        <div>button component here</div>
+      </div>
     </div>
   );
 }
